9dcdd3e4ca313744dfb5ca702396161db1293004
z <80871202+90th@users.noreply.github.com>
2023-03-09 20:52:48 -0500

    finished

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ProgramHASH {
+    internal class Program {
+        static void Main(string[] args) {
+            if (args.Length == 1) {
+                string filePath = args[0];
+
+                if (File.Exists(filePath)) {
+                    using (var md5 = MD5.Create()) {
+                        using (var stream = File.OpenRead(filePath)) {
+                            var hash = md5.ComputeHash(stream);
+                            Console.WriteLine($"HASH: {BitConverter.ToString(hash).Replace("-", "")}");
+                        }
+                    }
+                } else {
+                    Console.WriteLine("File not found.");
+                }
+            } else {
+                Console.WriteLine("Please drag a file onto the program's exe.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
